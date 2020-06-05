0f1232de469e27f29cc8eb1443ff63d844f06f22
Robert Važan <robert.vazan@protonmail.com>
2020-06-05 19:44:02 +0000

    Added a few tests

---
+++ src/test/java/com/machinezoo/hookless/servlets/ReactiveServletRequestTest.java
@@ -0,0 +1,45 @@
+// Part of Hookless Servlets: https://hookless.machinezoo.com
+package com.machinezoo.hookless.servlets;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import java.util.*;
+import javax.servlet.http.*;
+import org.junit.jupiter.api.*;
+
+public class ReactiveServletRequestTest {
+	@Test public void convert() {
+		HttpServletRequest hr = mock(HttpServletRequest.class);
+		when(hr.getRemoteAddr()).thenReturn("12.34.56.78");
+		when(hr.getRemotePort()).thenReturn(12345);
+		when(hr.getLocalAddr()).thenReturn("192.168.0.33");
+		when(hr.getLocalPort()).thenReturn(8080);
+		when(hr.getMethod()).thenReturn("POST");
+		when(hr.getRequestURL()).thenReturn(new StringBuffer("http://somplace/something"));
+		when(hr.getQueryString()).thenReturn("k1=v1&k2=v2");
+		when(hr.getHeaderNames()).thenReturn(Collections.enumeration(Arrays.asList("Header1", "Header2")));
+		when(hr.getHeaders("Header1")).thenReturn(Collections.enumeration(Arrays.asList("value")));
+		when(hr.getHeaders("Header2")).thenReturn(Collections.enumeration(Arrays.asList("value1", "value2")));
+		when(hr.getCookies()).thenReturn(new Cookie[] { new Cookie("n", "v") });
+		ReactiveServletRequest rq = new ReactiveServletRequest(hr);
+		assertEquals("12.34.56.78", rq.remoteAddress().getHostString());
+		assertEquals(12345, rq.remoteAddress().getPort());
+		assertEquals("192.168.0.33", rq.localAddress().getHostString());
+		assertEquals(8080, rq.localAddress().getPort());
+		assertEquals("POST", rq.method());
+		assertEquals("http://somplace/something?k1=v1&k2=v2", rq.url());
+		assertThat(rq.headers().keySet(), containsInAnyOrder("Header1", "Header2"));
+		assertEquals("value", rq.headers().get("header1"));
+		assertEquals("value1, value2", rq.headers().get("header2"));
+		assertEquals(1, rq.cookies().size());
+		assertEquals("n", rq.cookies().get(0).getName());
+		assertEquals("v", rq.cookies().get(0).getValue());
+	}
+	@Test public void caseInsensitive() {
+		ReactiveServletRequest rq = new ReactiveServletRequest();
+		rq.headers().put("HEADER", "value");
+		assertEquals("value", rq.headers().get("header"));
+	}
+}
---
+++ src/test/java/com/machinezoo/hookless/servlets/ReactiveServletResponseTest.java
@@ -0,0 +1,13 @@
+// Part of Hookless Servlets: https://hookless.machinezoo.com
+package com.machinezoo.hookless.servlets;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+public class ReactiveServletResponseTest {
+	@Test public void caseInsensitive() {
+		ReactiveServletResponse r = new ReactiveServletResponse();
+		r.headers().put("HEADER", "value");
+		assertEquals("value", r.headers().get("header"));
+	}
+}
