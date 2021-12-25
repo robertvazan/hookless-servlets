3e7391c13727c3c8320360915060a9f75304a0e4
Robert Važan <robert.vazan@tutanota.com>
2021-12-25 18:07:07 +0000

    Switched to Jetty modification of Jakarta servlet API

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,19 @@ module com.machinezoo.hookless.servlets {
 	exports com.machinezoo.hookless.servlets;
 	requires com.machinezoo.stagean;
 	requires com.machinezoo.hookless;
-	requires transitive jakarta.servlet;
+	/*
+	 * Transitive, because ReactiveServlet inherits from HttpServlet.
+	 * We are not using official Jakarta servlet API dependency, because it is reportedly broken.
+	 * https://github.com/eclipse/jetty.project/issues/6947
+	 * This will have to be fixed when Jakarta EE 10 and Jakarta servlet API 6 are out.
+	 * Meantime we are limited to deploying to Jetty server.
+	 */
+	requires transitive jetty.servlet.api;
 	requires org.apache.commons.collections4;
+	/*
+	 * SLF4J is pulled in transitively via noexception and then via hookless,
+	 * but the transitive dependency will be removed in future versions of noexception.
+	 */
+	requires org.slf4j;
+	requires micrometer.core;
 }
