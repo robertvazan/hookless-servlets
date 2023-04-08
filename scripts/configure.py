1efe9eb4211b7c32d48cd6d1f823953c6def3cea
Robert Važan <robert.vazan@tutanota.com>
2023-04-08 12:45:53 +0000

    Updated project configuration: build PRs, updated CONTRIBUTING.md, upgraded javadoc plugin and stagean, updated description, added UA banner

--- scripts/configure.py
+++ scripts/configure.py
@@ -1,34 +1,26 @@
 # This script generates and updates project configuration files.
 
-# We are assuming that project-config is available in sibling directory.
-# Checkout from https://github.com/robertvazan/project-config
-import os.path
-import sys
-sys.path.append(os.path.normpath(os.path.join(__file__, '../../../project-config/src')))
+# Run this script with rvscaffold in PYTHONPATH
+import rvscaffold as scaffold
 
-from java import *
+class Project(scaffold.Java):
+    def script_path_text(self): return __file__
+    def repository_name(self): return 'hookless-servlets'
+    def pretty_name(self): return 'Hookless Servlets'
+    def pom_description(self): return 'Reactive adapters for jakarta.servlet.* classes.'
+    def inception_year(self): return 2018
+    def stagean_annotations(self): return True
+    
+    def dependencies(self):
+        yield from super().dependencies()
+        yield self.use_hookless()
+        # We are not using official Jakarta servlet API dependency, because it is reportedly broken.
+        # https://github.com/eclipse/jetty.project/issues/6947
+        yield self.use('org.eclipse.jetty.toolchain:jetty-jakarta-servlet-api:5.0.2')
+        yield self.use_commons_collections()
+        yield self.use_junit()
+        yield self.use_hamcrest()
+        yield self.use_mockito()
+        yield self.use_slf4j_test()
 
-project_script_path = __file__
-repository_name = lambda: 'hookless-servlets'
-pretty_name = lambda: 'Hookless Servlets'
-pom_subgroup = lambda: 'hookless'
-pom_description = lambda: 'Reactive adapters for jakarta.servlet.* classes.'
-inception_year = lambda: 2018
-jdk_version = lambda: 11
-stagean_annotations = lambda: True
-homepage = lambda: website() + 'servlets'
-javadoc_site = lambda: website() + 'javadocs/servlets/'
-project_status = lambda: experimental_status()
-
-def dependencies():
-    use_hookless()
-    # We are not using official Jakarta servlet API dependency, because it is reportedly broken.
-    # https://github.com/eclipse/jetty.project/issues/6947
-    use('org.eclipse.jetty.toolchain:jetty-jakarta-servlet-api:5.0.2')
-    use_commons_collections()
-    use_junit()
-    use_hamcrest()
-    use_mockito()
-    use_slf4j_test()
-
-generate(globals())
+Project().generate()
